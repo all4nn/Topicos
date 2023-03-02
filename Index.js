e12e84249d905711d8fbabf55b0ad6dc2fa95e65
all4nn <88869328+all4nn@users.noreply.github.com>
2023-03-02 11:18:33 -0300

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,29 @@
+/*a = 10;
+b = 20;
+
+c = a + b;
+
+console.log(c);*/
+
+b1 = 2;
+b2 = 3;
+b3 = 4;
+b4 = 5;
+
+media = (b1+b2+b3+b4)/4;
+console.log(media);
+if(media >= 7){
+    console.log("Aprovado");
+}
+else {
+    if(media <= 2.5){
+        console.log("Reprovado");
+    }
+    else{
+        console.log("Recuperação")
+    }   
+    
+    for(i = 0; i < 10; i++){
+        console.log(i);
+    }
+}
